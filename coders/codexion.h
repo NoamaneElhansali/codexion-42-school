e5d757a7f6408d3fe201f2b77f6ddda47ce46fed
Noamane Elhansali <nelhansa@c4r2p3.1337.ma>
2026-04-08 14:30:15 +0100

    add fifo and edf

--- coders/codexion.h
+++ coders/codexion.h
@@ -26,6 +26,27 @@ typedef enum {
     EDF
 } t_scheduler;
 
+typedef struct s_queue
+{
+    int data[200];
+    int f;
+    int r;
+} t_queue;
+
+// typedef struct s_request
+// {
+//     int id;
+//     long deadline
+// } t_request;
+
+typedef struct s_heap
+{
+    t_coder *data[200];
+    int size;
+} t_heap;
+
+
+
 typedef struct s_table
 {
     int nb_coders;
@@ -33,16 +54,26 @@ typedef struct s_table
     long            time_to_compile;
     long            time_to_debug;
     long            time_to_refactor;
+    long            start_time;
     int must_compile;
     long dongle_cooldown;
     int stop;
     pthread_mutex_t *dongles;
     pthread_mutex_t print_lock;
     pthread_mutex_t stop_lock;
+    pthread_mutex_t queue_lock;
+    pthread_cond_t cond;
     t_scheduler scheduler;
+    pthread_t thread_monitor;
+    t_queue queue;
+    t_heap heap;
     t_coder *coders;
 
 } t_table;
+
+
+
+
 
 
 
@@ -56,5 +87,29 @@ void init_coders(t_table *table);
 int *parser(char **av, int ac);
 void init_mutex(t_table *table);
 t_table *init_parameter(int *paramter, char *scheduler);
+long gettimenow();
+int get_stop(t_table *table);
+void set_stop(t_table *table);
+void start_monitoring(t_table *table);
+void *monitor(void *arg);
+void debugging(t_coder *coder);
+void refactoring(t_coder *coder);
+void take_dongles(t_coder *coder);
+void give_dongles(t_coder *coder);
+void join_coders(t_table *table);
+void push(t_queue *q, int id);
+int pop(t_queue *q);
+void init_queue(t_queue *q);
+int take_dongles_fifo(t_coder *coder);
+void release_dongles_fifo(t_coder *coder);
+int coder_is_burned(t_coder *coder);
+long get_deadline(t_coder *coder);
+void add_to_heap(t_heap *heap, t_coder *coder);
+void remove_min(t_heap *heap);
+t_coder *get_min_heap(t_heap *heap);
+int take_dongles_edf(t_coder *coder);
+void release_dongles_edf(t_coder *coder);
+int is_in_first_fifo(t_queue *q, int id, int n);
+int is_in_first_edf(t_heap *h, int id, int n);
 
 # endif
---
+++ coders/fifo.c
@@ -0,0 +1,58 @@
+#include "codexion.h"
+
+
+void push(t_queue *q, int id)
+{
+    q->data[q->r++] = id;
+}
+
+
+int pop(t_queue *q)
+{
+    return q->data[q->f++];
+}
+
+void init_queue(t_queue *q)
+{
+    q->f = 0;
+    q->r = 0;
+}
+
+int take_dongles_fifo(t_coder *coder)
+{
+    t_table *t = coder->table;
+    pthread_mutex_lock(&t->queue_lock);
+    push(&t->queue, coder->id);
+    while (!is_in_first_fifo(&t->queue, coder->id, t->nb_coders / 2) && !get_stop(t))
+        pthread_cond_wait(&t->cond, &t->queue_lock);
+    pthread_mutex_unlock(&t->queue_lock);
+    if (get_stop(t))
+        return 0;
+    take_dongles(coder);
+    return 1;
+}
+
+void release_dongles_fifo(t_coder *coder)
+{
+    give_dongles(coder);
+    pthread_mutex_lock(&coder->table->queue_lock);
+    pop(&coder->table->queue);
+    pthread_cond_broadcast(&coder->table->cond);
+    pthread_mutex_unlock(&coder->table->queue_lock);
+}
+
+int is_in_first_fifo(t_queue *q, int id, int n)
+{
+    int i = q->f;
+    int count = 0;
+
+    while (i < q->r && count < n)
+    {
+        while (q->data[i] == id)
+            return (1);
+        
+        count++;
+        i++;
+    }
+    return (0);
+}
